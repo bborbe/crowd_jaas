57c683654627ee833e01bf77b7d9040a24a95adb
Benjamin Borbe <bborbe@rocketnews.de>
2016-03-01 17:51:00 +0100

    add files

---
+++ src/main/java/de/benjaminborbe/jaas/crowd/RolePrincipal.java
@@ -0,0 +1,23 @@
+package de.benjaminborbe.jaas.crowd;
+
+import java.security.Principal;
+
+public class RolePrincipal implements Principal {
+	
+	private String name;
+	
+	public RolePrincipal(String name) {
+		super();
+		this.name = name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public String getName() {
+		return name;
+	}
+
+}
